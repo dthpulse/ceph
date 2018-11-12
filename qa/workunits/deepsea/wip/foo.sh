c585c0d717e982d89b9c339350c4e78557d6660c
Denis Polom <dpolom@suse.de>
2018-11-12 13:29:31 +0100

    rados test
    Signed-off-by: Denis Polom <dpolom@suse.de>

--- qa/workunits/deepsea/wip/foo.sh
+++ qa/workunits/deepsea/wip/foo.sh
@@ -2,9 +2,10 @@
 set -x
 WORKUNITS_DIR=$CEPH_BASE/qa/workunits
 
-rados --striper
+sudo rados --striper
 RETURN_VALUE=$?
 set -e
 test "$RETURN_VALUE" != 0
-rados --striper 2>&1 | tee test-striper-output
+sudo rados --striper 2>&1 | tee test-striper-output
 grep "unrecognized command --striper" test-striper-output
+
